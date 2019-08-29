c1ab955e75bef3ecb3a9497413d522c763df3b25
Cristiano Gonçalves da Silva Braga <40774358+CPrimbee@users.noreply.github.com>
2019-08-29 13:08:28 -0300

    Novo projeto

---
+++ scriptBasico.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding: utf-8
+print "O cabeçalho em script é: #!/usr/bin/env python ou #!/usr/bin/python"
+print "Para não aparecer o erro (SyntaxErro: Non-ASCII character), use essa linha logo abaixo do cabeçalho: #coding :utf-8"
+print "Os comentários começam com: #"
+print "Para imprimir na tela, ex.: print Olá, mundo!"
+print "Não precisa do ';' no final, mas coloquei aqui e não apresentou erro;";
+print "Pode ter comentários no meio da linha como aqui e não aparece" # comentário
+variavel = "Minha Variável"
+print variavel
+print "Cria-se variáveis assim: variavel = 'Minha Variável' , pode usar aspas simples ou duplas"
+soma = 8+8;
+print "Operações aritiméticas (soma+, subtração-, multiplicação*, divisão/): print 8+8 = ",soma
+print "Para concatenar strings basta usar o +, ex.: print 'Pala'+'vra'"
+print 'Pala'+'vra'
+print "Mas pra concatenar strins e integer, use-se vírgula(,) (soma é uma variável/integer 8+8), ex.: print '8+8', soma"
+print "Pode-se usar a vírgula para separar também strings, no entanto, ficará com espaço, ex.: 'Pala','vra' --> Pala vra"
+"comentário aspas duplas"
+'comentário aspas simples'
+print "Comentários também podem ser entre aspas simples ou duplas, como há nesse script, mas não pode misturá-los num mesmo comentário"
+print 7/2
+print 7.0/2
+print 'Na divisão quando quiser obter números irracionais(float), tem de fazer também com float, como exemplo acima'
